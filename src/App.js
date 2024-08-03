5c96e81238d2e399ac618bf989c7ff5e0d068f27
majeed4u <jamodah20@gmail.com>
2024-08-03 16:57:23 +0300

    updated jenkinsfile pipline with adding node18 and run build

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
-  function increaseCount() {
-    setCount(count + 1)
-  }
-  function decreaseCount() {
-    setCount(count - 1)
-  }
+  // const [count, setCount] = useState(0)
+  // function increaseCount() {
+  //   setCount(count + 1)
+  // }
+  // function decreaseCount() {
+  //   setCount(count - 1)
+  // }
   return (
     <div className="App">
       <header className="App-header">
